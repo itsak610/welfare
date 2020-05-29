095985762b6b573525f30bf9d4942207da31a7b6
ItsAkBxtches <akshat.namdeo.7070@gmail.com>
2020-05-29 17:11:29 +0530

    added nav bar for mobile devices and media queries for all remaining platforms

---
+++ resources/scripts/script1.js
@@ -0,0 +1,13 @@
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.height = "100vh";
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("body").style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.height = "0";
+    document.getElementById("mySidenav").style.width = "0";
+}
